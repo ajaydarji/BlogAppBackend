9c05e4d55aea64a6873dfd6985d5c143d01bd83d
ajaydarji <adarji038@gmail.com>
2024-11-04 21:04:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://blog-app-mern-proj.netlify.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
